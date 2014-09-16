9c929b94d205277283388539956cfcf36b4cbfc9
PT Guy <PT Guy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-16 21:46:03 +0000

    Release version 1.3.1.8
    git-svn-id: https://plugins.svn.wordpress.org/content-views-query-and-display-post-page/tags/1.3.1.8@991437 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/includes/templates/settings-section-two.php
@@ -0,0 +1,135 @@
+<hr>
+<p>Here is list of some of awesome features which are available at
+	<a href="http://www.contentviewspro.com/?utm_source=settings_page&utm_medium=link&utm_campaign=content-views" target="_blank">Content Views PRO</a>
+</p>
+
+<p>
+	<a href="http://www.contentviewspro.com/pricing/?utm_source=settings_page&utm_medium=link&utm_campaign=content-views" target="_blank" class="btn btn-success">Upgrade now</a>
+	or <a href="http://sample.contentviewspro.com/?utm_source=client&utm_medium=view" target="_blank" class="btn btn-info">Check demo site</a>
+</p>
+
+<div class="row">
+	<div class="col-md-6">
+		<h3>
+			Query custom post types
+		</h3>
+
+		<div>
+			<p>You can get any posts of any custom post types (<strong>WooCommerce</strong> product, FAQ, Book...)</p>
+
+			<p>Especially, with <strong>WooCommerce</strong>, there are quick filters to pick up</p>
+			<ul style="list-style: circle; margin-left: 20px;">
+				<li>Recent products</li>
+				<li>Best selling products</li>
+				<li>Features products</li>
+				<li>Top rated products</li>
+			</ul>
+			easily
+		</div>
+	</div>
+	<div class="col-md-6">
+		<div>
+			<img src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/woocommerce.png', PT_CV_FILE ) ); ?>" />
+		</div>
+	</div>
+</div>
+
+<hr>
+
+<div class="row">
+	<div class="col-md-12">
+		<h3>
+			More beautiful & awesome layouts
+		</h3>
+
+		<div>
+			<div class="row">
+				<div class="col-md-6">
+					<h6>Scrollable list</h6>
+
+					<div>
+						<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/scrollable.png', PT_CV_FILE ) ); ?>" />
+					</div>
+				</div>
+				<div class="col-md-6">
+					<h6>Collapsible list</h6>
+
+					<div>
+						<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/collapsible.png', PT_CV_FILE ) ); ?>" />
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<h6>Pinterest</h6>
+
+					<div>
+						<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/pinterest.png', PT_CV_FILE ) ); ?>" />
+					</div>
+				</div>
+				<div class="col-md-6">
+					<h6>Timeline</h6>
+
+					<div>
+						<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/timeline.png', PT_CV_FILE ) ); ?>" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<hr>
+
+<div class="row">
+	<div class="col-md-6">
+		<h3>
+			Drag & drop to change display order of fields
+		</h3>
+
+		<div>
+			<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/drag_drop.png', PT_CV_FILE ) ); ?>" />
+		</div>
+	</div>
+	<div class="col-md-6">
+		<h3>
+			Read more settings
+		</h3>
+
+		<div>
+			<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/readmore.png', PT_CV_FILE ) ); ?>" />
+		</div>
+	</div>
+</div>
+
+<hr>
+
+<div class="row">
+	<div class="col-md-6">
+		<h3>
+			Custom font settings
+		</h3>
+
+		<div>
+			<p>You can customize Font family (from top 50 most popular Google fonts), font style, font size for Title, Content, Meta fields easily</p>
+			<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/font-settings.png', PT_CV_FILE ) ); ?>" style="height: 210px;" />
+		</div>
+	</div>
+	<div class="col-md-6">
+		<h3>
+			And more powerful options
+		</h3>
+
+		<div>
+			<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/pagination.png', PT_CV_FILE ) ); ?>" />
+			<img class="thumbnail" src="<?php echo esc_url( plugins_url( 'admin/assets/images/features/openin.png', PT_CV_FILE ) ); ?>" />
+			<br> <span style="font-size: 30px;">...</span>
+		</div>
+	</div>
+</div>
+
+<hr>
+
+<p>I hope you enjoy with Content Views!</p>
+<p>Plugin developed by PT Guy (support@contentviewspro.com)</p>
+<p>Copyright &COPY; 2014</p>
