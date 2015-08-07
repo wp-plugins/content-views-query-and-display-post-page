c362fcd83c990828ef062168a2ec7486b1925c3d
PT Guy <PT Guy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-07 11:12:41 +0000

    Release version 1.6.5.1
    git-svn-id: https://plugins.svn.wordpress.org/content-views-query-and-display-post-page/tags/1.6.5.1@1214781 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/update.php
+++ includes/update.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Check update, do update
  *
@@ -8,12 +9,10 @@
  * @link      http://www.contentviewspro.com/
  * @copyright 2014 PT Guy
  */
-
 // Compare stored version and current version
 $stored_version = get_option( PT_CV_OPTION_VERSION );
 if ( $stored_version && version_compare( $stored_version, PT_CV_VERSION, '<' ) ) {
 	// Do update
-
 	// Update version
 	update_option( PT_CV_OPTION_VERSION, PT_CV_VERSION );
 }
